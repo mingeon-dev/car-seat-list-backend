9aa4099006bad5eddf962ba41f448ae5fd0c827e
Mingeon kim <mingeon.dev@gmail.com>
2020-05-04 21:37:47 +0900

    fix: deprecated 메소드 수정

--- src/createProducts.js
+++ src/createProducts.js
@@ -2,6 +2,6 @@ import Product from './models/product';
 import products from './products.js';
 
 export default async function createProducts() {
-  await Product.remove({});
+  await Product.deleteMany({});
   Product.insertMany(products);
 }
